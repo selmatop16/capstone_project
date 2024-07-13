16602a35f2c2edb565151e88b8ce9221c0900e44
selmatop <selmatop16@gmail.com>
2024-07-13 11:39:52 -0400

    first

---
+++ src/test/java/PageObjects/DashboardPage.java
@@ -0,0 +1,20 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage {
+   @FindBy(id = "menu_dashboard_index")
+    WebElement dashboard;
+    WebDriver driver;
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+   //Verifying if dashboard is displayed
+    public void checkDashboardIsDisplayed(){
+        dashboard.isDisplayed();
+    }
+}
