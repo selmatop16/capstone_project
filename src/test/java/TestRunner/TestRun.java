b1946e974bf1e91e6f96809d6be5cb448bdab471
selmatop <selmatop16@gmail.com>
2024-07-14 13:48:00 -0400

    login

---
+++ src/test/java/TestRunner/TestRun.java
@@ -0,0 +1,17 @@
+package TestRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="src/test/resources/Features"
+        ,glue="StepDefinitions"
+        ,dryRun=false
+        ,plugin= {"pretty","html:test-output"}
+        ,monochrome=true
+)
+public class TestRun {
+
+}
