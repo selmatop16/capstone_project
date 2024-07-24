5bcec80fc0c23586989ce41a98e332f2e5fab491
selmatop <selmatop16@gmail.com>
2024-07-24 18:22:10 -0400

    working project 2

--- src/test/java/StepDefinitions/BaseClass.java
+++ src/test/java/StepDefinitions/BaseClass.java
@@ -5,16 +5,46 @@ import PageObjects.DashboardPage;
 import PageObjects.LoginPage;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
-    WebDriver driver;
-    LoginPage login;
-    DashboardPage dashboardPage;
-    AdminPage adminPage;
 
-    //created for generating random string for unique email
-    public static String randomestring(){
-        String generatedString1 = RandomStringUtils.randomAlphabetic(5);
-        return (generatedString1);
+    static WebDriver driver;
+    public LoginPage loginPage;
+    public DashboardPage dashboardPage;
+    AdminPage adminPage;
+    public BaseClass() {
+        // Initialize WebDriver
+     /*   System.setProperty("webdriver.gecko.driver","/Users/selmatop/workspace/capstone_project/src/test/Drivers/geckodriver");
+        driver = new FirefoxDriver();
+
+        // Initialize page objects
+        loginPage = new LoginPage(driver);
+        dashboardPage = new DashboardPage(driver);
+        adminPage = new AdminPage(driver);*/
+
+
+
+
+        if (driver == null) {
+            // Initialize WebDriver only if it's not already initialized
+            System.setProperty("webdriver.gecko.driver", "/Users/selmatop/workspace/capstone_project/src/test/Drivers/geckodriver");
+            driver = new FirefoxDriver();
+        }
+
+        // Initialize page objects
+        loginPage = new LoginPage(driver);
+        dashboardPage = new DashboardPage(driver);
+        adminPage = new AdminPage(driver);
     }
-}
+
+    // Method to quit the driver
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    }
+
